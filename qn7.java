d7c26d69be88785a719866c2ab41eeea2d19861a
AdityaThapa1 <102941877+AdityaThapa1@users.noreply.github.com>
2022-09-20 10:44:58 +0545

    quadratic eqn using java

---
+++ qn7.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class sqroot
+{
+    public static void main(String args[]) 
+    {
+        double a,b,c,x,y;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter A: ");
+        a = scan.nextDouble();
+        System.out.print("Enter B : ");
+        b = scan.nextDouble();
+        System.out.print("Enter C : ");
+        c = scan.nextDouble();
+        scan.close();
+	double d= b * b - 4.0 * a * c;  
+        x= ((-b)+(Math.pow(d,0.5)))/(2.0*a);
+	y= ((-b)-(Math.pow(d,0.5)))/(2.0*a);
+        System.out.printf("Quadratic root are:\n");
+	System.out.printf(" %.2f \n",x);
+	System.out.printf("Quadratic root are: is: %.2f ",y);
+    }
+}
