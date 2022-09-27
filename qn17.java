a1e9915a17a6532ef5e8ce53d29917be926a6160
AdityaThapa1 <102941877+AdityaThapa1@users.noreply.github.com>
2022-09-27 08:27:26 +0545

    2nd pattern using java

---
+++ qn17.java
@@ -0,0 +1,19 @@
+
+public class pattern2	 {
+    
+  public static void main(String args[]) {
+	int i,j,k;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=(5-i);j++){
+			System.out.printf(" ");
+		}
+
+	for(k=1;k<=i;k++){
+		 System.out.printf("*");
+	}
+        System.out.printf("\n");
+    }
+}
+}
+	
+	
